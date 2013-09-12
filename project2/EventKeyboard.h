211f8ce035998e69d943ffbb5ef36fb97adaa967
Benjamin Sautermeister <apps@bsautermeister.de>
2013-09-12 01:41:34 -0400

    Added and implemented keyboard and mouse event classes

---
+++ project2/EventKeyboard.h
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * @file        EventKeyboard.h
+ * @author      bsautermeister
+ * @description Keyboard event class to notify other game when
+ *              a key was pressed.
+ ******************************************************************************/
+
+#ifndef __EVENT_KEYBOARD_H__
+#define __EVENT_KEYBOARD_H__
+
+#include "Event.h"
+
+#define KEYBOARD_EVENT "__keyboard__"
+
+#define KEY_UNDEFINED -1;
+
+/**
+ * The keyboard event class.
+ */
+class EventKeyboard : public Event
+{
+private:
+	/**
+	 * The keyboard value.
+	 */
+	int _keyValue;
+public:
+	/**
+	 * Creates a new keyboard event instance.
+	 */
+	EventKeyboard(void);
+
+	/**
+	 * Gets the pressed key of the event.
+	 * @return The pressed key.
+	 */
+	int getKey(void);
+
+	/**
+	 * Sets the pressed key of the event.
+	 * @param key The pressed key.
+	 */
+	void setKey(int key);
+};
+
+#endif
