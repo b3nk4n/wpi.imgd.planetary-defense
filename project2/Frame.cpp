e5c54e2f03ab9f837009117e161989451eecc21f
Benjamin Sautermeister <apps@bsautermeister.de>
2013-09-15 22:55:37 -0400

    Added a simple frame class

---
+++ project2/Frame.cpp
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * @file        Frame.cpp
+ * @author      bsautermeister
+ * @description The text based 2D frame for an animated game object.
+ ******************************************************************************/
+
+#include "Frame.h"
+
+/**
+ * Creates the instance of an empty frame.
+ */
+Frame::Frame(void)
+{
+	setWidth(0);
+	setHeight(0);
+	setData(EMTPY_FRAME);
+}
+
+/**
+ * Creates a frame instance.
+ * @param width The frame width.
+ * @param height The frame height.
+ * @param data The frame string data.
+ */
+Frame::Frame(int width, int height, string data)
+{
+	setWidth(width);
+	setHeight(height);
+	setData(data);
+}
+
+/**
+ * Gets the frame width.
+ * @return The frame width.
+ */
+int Frame::getWidth(void)
+{
+	return _width;
+}
+
+/**
+ * Sets the frame width.
+ * @param value The frame width.
+ */
+void Frame::setWidth(int value)
+{
+	_width = value;
+}
+
+/**
+ * Gets the frame height.
+ * @return The frame height.
+ */
+int Frame::getHeight(void)
+{
+	return _height;
+}
+
+/**
+ * Sets the frame height.
+ * @param value The frame height.
+ */
+void Frame::setHeight(int value)
+{
+	_height = value;
+}
+
+/**
+ * Gets the frame data.
+ * @return The frame string data.
+ */
+string Frame::getData(void)
+{
+	return _data;
+}
+
+/**
+ * Sets the frame data.
+ * @param data The string frame data.
+ */
+void Frame::setData(string data)
+{
+	_data = data;
+}
